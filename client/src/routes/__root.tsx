0b8094fa2383293a9d9e78ba91dd140ba58620c8
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-07-13 20:37:22 +0700

    feat(client): update layout

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,4 +1,5 @@
-import { Icon } from "@iconify/react";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 // import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
@@ -9,23 +10,13 @@ export const Route = createRootRoute({
 function RootLayout() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen w-screen">
-      <header className="w-full p-4 text-center bg-black text-gray-100">
-        <h1 className="text-xl">BomSLap - BERM stack</h1>
-      </header>
+      <Navbar />
 
       <main className="p-2">
         <Outlet />
       </main>
 
-      <footer className="w-full p-2 bg-black text-gray-100">
-        <a
-          className="flex gap-2"
-          href="https://github.com/sompakorn-lap/basic-setup-berm-stack"
-        >
-          <Icon icon="mdi:github" width={24} height={24} />
-          <span>GITHUB</span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
